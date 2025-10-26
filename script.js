f13196f3230b54ea3754b3deb2157f903437bb7a
Ana Goaten <anagoetentd@gmail.com>
2025-10-26 14:22:22 -0300

    finished

--- script.js
+++ script.js
@@ -7,4 +7,20 @@ function menuShow() {
         menuMobile.classList.add('open');
         document.querySelector('.hamburger').src="./assets/tabler-icon-x.png"
     }
-}
+};
+
+const hiden = document.querySelectorAll('.hiden');
+
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach( (entry) => {
+        if (entry.isIntersecting){
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove ('show')
+        }
+    })
+})
+
+hiden.forEach(element => {
+    myObserver.observe(element)
+})
